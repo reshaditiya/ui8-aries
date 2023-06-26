c969147bc91c14684a770321be50fb5ab40cbe46
reshaditiya <reshaditiya@gmail.com>
2023-06-26 10:39:04 +0700

    component to components

---
+++ src/components/ArrowButton.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image"
+
+export function ArrowButton({ title }: { title: string }) {
+	return (
+		<div className="">
+			<button className="flex items-center gap-[0.63rem] font-display font-[100] uppercase italic text-primary transition-all">
+				{title + " "}
+				<figure className="relative h-[0.75rem] w-[1.5rem]">
+					<Image src="/icon/arrow.svg" fill alt="" />
+				</figure>
+			</button>
+		</div>
+	)
+}
