27bd6b78424360450289a09e53c72f416c32e04c
reshaditiya <reshaditiya@gmail.com>
2023-06-26 10:41:23 +0700

    hover animation

--- src/components/ArrowButton.tsx
+++ src/components/ArrowButton.tsx
@@ -3,9 +3,9 @@ import Image from "next/image"
 export function ArrowButton({ title }: { title: string }) {
 	return (
 		<div className="">
-			<button className="flex items-center gap-[0.63rem] font-display font-[100] uppercase italic text-primary transition-all">
+			<button className="group flex items-center gap-[0.63rem] font-display font-[100] uppercase italic text-primary">
 				{title + " "}
-				<figure className="relative h-[0.75rem] w-[1.5rem]">
+				<figure className="relative h-[0.75rem] w-[1.5rem] transition-all group-hover:translate-x-[0.5rem]">
 					<Image src="/icon/arrow.svg" fill alt="" />
 				</figure>
 			</button>
