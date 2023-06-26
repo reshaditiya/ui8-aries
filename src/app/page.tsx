b106a44913a24e61d9cd9abdba2f6911cbdfb608
reshaditiya <reshaditiya@gmail.com>
2023-06-26 09:22:04 +0700

    button color name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,153 @@
-import Image from 'next/image'
+import Image from "next/image"
+import Link from "next/link"
+import { ArrowButton } from "@/component"
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	return (
+		<main>
+			{/* navbar */}
+			<nav className="fixed top-0 z-10 w-full md:top-[1.88rem] md:px-[2.5rem]">
+				<div className="mx-auto flex max-w-[72.5rem] items-center bg-secondary-dark px-[1.25rem] py-[2rem] md:px-[3.75rem] md:py-[1.25rem]">
+					<span className="hidden md:flex md:gap-[1.88rem]">
+						<Link href="program">PROGRAM</Link>
+						<Link href="program">FUNDING</Link>
+					</span>
+					<span className="flex flex-1 md:justify-center">
+						<figure className="relative h-[1.56rem] w-[5.31rem] md:h-[2.5rem] md:w-[8.62rem]">
+							<Image src="/logo.svg" alt="Aries Logo" fill />
+						</figure>
+					</span>
+					<span className="hidden md:flex md:gap-[1.88rem]">
+						<Link href="program">JOBS</Link>
+						<span className="flex items-center gap-[0.5rem]">
+							<Link href="program">MENU</Link>
+							<figure className="relative h-[0.75rem] w-[0.75rem]">
+								<Image
+									src="/icon/menu.svg"
+									alt="Aries Logo"
+									fill
+								/>
+							</figure>
+						</span>
+					</span>
+					{/* menu icon mobile */}
+					<figure className="relative h-[1.25rem] w-[1.25rem] md:hidden">
+						<Image src="/icon/menu.svg" alt="Aries Logo" fill />
+					</figure>
+				</div>
+			</nav>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+			{/* hero title */}
+			<section className="relative mx-auto pb-[11.88rem] pt-[11.88rem] md:pb-[13rem] md:pt-[13rem]">
+				<div className="mx-auto max-w-[72.5rem] px-[1.25rem] md:px-[2.5rem]">
+					<h1 className="text-h1 md:text-center">
+						We Are Here For Your{" "}
+						<div className="angular-gradient inline-block bg-clip-text text-transparent">
+							Artificial Intelligence
+						</div>{" "}
+						Startup.
+					</h1>
+					<figure className="relative mx-auto hidden h-[3.62rem] w-[0.75rem] md:mt-[3rem] md:block">
+						<Image
+							src="/icon/scroll-arrow.svg"
+							fill
+							alt="Artificial Face"
+						/>
+					</figure>
+				</div>
+				<Image
+					src="/images/hero-pattern.webp"
+					fill
+					alt=""
+					className="-z-10 object-cover"
+				/>
+			</section>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+			{/* program section */}
+			<section className="relative mx-auto mt-[2rem] max-w-[72.5rem] px-[1.25rem] md:px-[2.5rem]">
+				<div className="relative">
+					<figure className="relative mr-[1.88rem] h-[10rem] max-w-[55.81rem] md:mx-auto md:h-[30.5rem]">
+						<Image
+							src="/images/home/program-section/program-1.webp"
+							fill
+							alt="Artificial Face"
+							className="object-cover"
+						/>
+					</figure>
+					<figure className="absolute right-0 top-[1.27rem] h-[7.25rem] w-[5.31rem] md:top-[3.75rem] md:h-[23rem] md:w-[16.75rem]">
+						<Image
+							src="/images/home/program-section/program-2.webp"
+							fill
+							alt="AI Developer"
+						/>
+					</figure>
+				</div>
+				<div className="mt-[2.5rem] flex max-w-[32.62rem] flex-col gap-[1.88rem] md:absolute md:top-[19.44rem] md:mt-0 md:gap-[3.75rem]">
+					<h2 className="text-h2">
+						building faster, smarter,together.
+					</h2>
+					<p className="text-secondary-title">
+						ARIES is a part of PIXIRHY, a global venture capital
+						firm providing multi stage investment to develop and
+						scale our founders’ big ideas. SOSV invests in every HAX
+						program startup with follow-on through pre-iPO.
+					</p>
+					<ArrowButton title="VIEW PROGRAM" />
+				</div>
+			</section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+			{/* invest program */}
+			<section className="relative mx-auto mt-[6.25rem] max-w-[72.5rem] items-center px-[1.25rem] md:mt-[25.19rem] md:px-[2.5rem]">
+				<div className="flex flex-col gap-[1.88rem] md:flex-row md:items-center md:gap-[3.37rem]">
+					<h2 className="text-h2 md:flex-1">
+						We invest $27 Billion per year into AI startups.
+					</h2>
+					<p className="max-w-[27.62rem] text-secondary-title md:flex-1">
+						They are showing researches and writting regularly about
+						the consequnces happening on AI. Our unique program is
+						designed for startups, combining hands-on help from a
+						deeply experienced product development team and a total
+						investment package of $270,000.
+					</p>
+				</div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+				{/* invest card */}
+				<div className="relative mt-[3.75rem] flex flex-col gap-[3.12rem] md:flex-row">
+					<div className="relative md:flex-1">
+						<figure className="relative mr-[1.88rem] h-[13.75rem] md:mx-auto md:h-[30rem]">
+							<Image
+								src="/images/home/invest-program/kawaski-1.webp"
+								fill
+								alt="Sphere Texture"
+								className="object-cover"
+							/>
+						</figure>
+						<figure className="absolute right-0 top-[1.88rem] h-[13.75rem] w-[9.37rem] md:top-[3.75rem] md:h-[23rem] md:w-[16.75rem]">
+							<Image
+								src="/images/home/invest-program/kawaski-2.webp"
+								fill
+								alt="AI Developer"
+							/>
+						</figure>
+					</div>
+					<div className="flex max-w-[32.62rem] flex-1 flex-col gap-[1.75rem] md:gap-[2.83rem]">
+						<header className="flex flex-col gap-[0.38rem]">
+							<span className="font-display tracking-[0.3rem] text-secondary-title/70">
+								KAWASKI
+							</span>
+							<h2 className="text-h3">
+								Corporobo Design Interview with Ben Lukas.
+							</h2>
+						</header>
+						<p className="text-secondary-title">
+							We spoke to Robotic and sound designer Ben Lukas
+							Boysen about the evolution of robot soundtracks and
+							some of his top picks.
+						</p>
+						<ArrowButton title="VIEW PROJECT" />
+					</div>
+				</div>
+			</section>
+		</main>
+	)
 }
