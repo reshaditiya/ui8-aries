91f90f4a3e161e42173f5fc94c23a6b5ba971cfa
reshaditiya <reshaditiya@gmail.com>
2023-06-26 08:56:33 +0700

    setting color and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				"primary-dark": "#1D1D1D",
+				"secondary-dark": "#232526",
+				"tertiary-dark": "#30363A",
+				"pattern-dark": "#000000",
+				"primary-title": "#F4F4F4",
+				"secondary-title": "#B8B8B8",
+				primary: "#99A9FF",
+				secondary: "#5CE4CF",
+			},
+			fontFamily: {
+				display: ["var(--font-josefin-sans)", "sans"],
+				body: ["var(--font-montserrat)", "sans"],
+			},
+		},
+	},
 }
