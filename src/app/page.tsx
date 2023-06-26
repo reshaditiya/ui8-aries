b369d93187241963fd905cf6169fc7f89a0f394e
reshaditiya <reshaditiya@gmail.com>
2023-06-26 10:58:39 +0700

    move hero title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import { ArrowButton, Navbar } from "@/components"
+import { HeroTitle } from "@/sections/home"
 
 export default function Home() {
 	return (
@@ -7,30 +8,7 @@ export default function Home() {
 			<Navbar />
 
 			{/* hero title */}
-			<section className="relative mx-auto pb-[11.88rem] pt-[11.88rem] md:pb-[13rem] md:pt-[13rem]">
-				<div className="mx-auto max-w-[72.5rem] px-[1.25rem] md:px-[2.5rem]">
-					<h1 className="text-h1 md:text-center">
-						We Are Here For Your{" "}
-						<div className="angular-gradient inline-block bg-clip-text text-transparent">
-							Artificial Intelligence
-						</div>{" "}
-						Startup.
-					</h1>
-					<figure className="relative mx-auto hidden h-[3.62rem] w-[0.75rem] animate-bounce md:mt-[3rem] md:block">
-						<Image
-							src="/icon/scroll-arrow.svg"
-							fill
-							alt="Artificial Face"
-						/>
-					</figure>
-				</div>
-				<Image
-					src="/images/hero-pattern.webp"
-					fill
-					alt=""
-					className="-z-10 object-cover"
-				/>
-			</section>
+			<HeroTitle />
 
 			{/* program section */}
 			<section className="relative mx-auto mt-[2rem] max-w-[72.5rem] px-[1.25rem] md:px-[2.5rem]">
---
+++ src/sections/home/HeroTitle.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image"
+
+export function HeroTitle({}) {
+	return (
+		<section className="relative mx-auto pb-[11.88rem] pt-[11.88rem] md:pb-[13rem] md:pt-[13rem]">
+			<div className="mx-auto max-w-[72.5rem] px-[1.25rem] md:px-[2.5rem]">
+				<h1 className="text-h1 md:text-center">
+					We Are Here For Your{" "}
+					<div className="angular-gradient inline-block bg-clip-text text-transparent">
+						Artificial Intelligence
+					</div>{" "}
+					Startup.
+				</h1>
+				<figure className="relative mx-auto hidden h-[3.62rem] w-[0.75rem] animate-bounce md:mt-[3rem] md:block">
+					<Image
+						src="/icon/scroll-arrow.svg"
+						fill
+						alt="Artificial Face"
+					/>
+				</figure>
+			</div>
+			<Image
+				src="/images/hero-pattern.webp"
+				fill
+				alt=""
+				className="-z-10 object-cover"
+			/>
+		</section>
+	)
+}
